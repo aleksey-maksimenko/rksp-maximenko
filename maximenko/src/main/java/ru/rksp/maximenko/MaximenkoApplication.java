2770a7f6bec628a7453cbcbb22e358ab49671fac
aleksey-maksimenko <62542456+aleksey-maksimenko@users.noreply.github.com>
2025-10-31 22:01:46 +0300

    Выполнена генерация сервиса на Java

---
+++ maximenko/src/main/java/ru/rksp/maximenko/MaximenkoApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.maximenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximenkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximenkoApplication.class, args);
+	}
+
+}
---
+++ maximenko/src/test/java/ru/rksp/maximenko/MaximenkoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.maximenko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaximenkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
