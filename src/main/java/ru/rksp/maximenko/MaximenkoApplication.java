325690a868ee2d8531e3d8470f29fe3dab30fdb6
aleksey-maksimenko <62542456+aleksey-maksimenko@users.noreply.github.com>
2025-11-11 10:18:03 +0300

    Добавили Luqibase

---
+++ src/main/java/ru/rksp/maximenko/MaximenkoApplication.java
@@ -0,0 +1,14 @@
+package ru.rksp.maximenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximenkoApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(MaximenkoApplication.class, args);
+        System.out.println("ok");
+	}
+
+}
